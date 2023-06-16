4f694dfb64afb7e831d6e3fc9d277384b332bd35
sheldon05 <jesussantoscapote@gmail.com>
2023-06-15 23:35:38 -0400

    Update test_cases_generator.py

--- src/test_cases_generator.py
+++ src/test_cases_generator.py
@@ -4,11 +4,24 @@ def generator(num_clauses, num_var, cases_number):
     vars = [i for i in range(1, num_var + 1)]
     used_vars = [False for i in vars]
     test_cases = []
-    clauses_not_full = [i for i in range(num_clauses)]
 
     for i in range(cases_number):
         cnf = [[] for i in range(num_clauses)]
-        for j in vars:
+        clauses_not_full = [i for i in range(num_clauses)]
+
+        for var in vars:
             clause = random.choice(clauses_not_full)
+            cnf[clause].append(var)
+            if len(cnf[clause]) == 3:
+                clauses_not_full.remove(clause)
+
+        for clause in clauses_not_full:
+            missing_terms = 3 - len(cnf[clause])
+            vars_unused = [var for var in vars]
+            
+            for var in cnf[clause]:
+                vars_unused.remove(var)
+
+            for
             
             
